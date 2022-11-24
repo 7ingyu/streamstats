e9080c3397983a2e95922f85d28eaa3d278776cb
Ting-Yu Chen <7ingyu@gmail.com>
2022-11-23 21:02:49 -0800

    Passing statistics to react dashboard

--- app/Http/Controllers/TwitchController.php
+++ app/Http/Controllers/TwitchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\TopStream;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\Request;
 use Illuminate\Http\Client\RequestException;
@@ -137,5 +138,123 @@ class TwitchController extends Controller
             ->throw();
         $data = $stream_res->json();
         return $data['data'];
+    }
+
+    /**
+     * Get top streams
+     *
+     * @param array $tags
+     * @return array $user_data
+     */
+    public static function getTagNames($tags)
+    {
+        // Get Access Token
+        $auth_res = Http::post('https://id.twitch.tv/oauth2/token', [
+            'client_id' => env('TWITCH_CLIENT_ID'),
+            'client_secret' => env('TWITCH_CLIENT_SECRET'),
+            'grant_type' => 'client_credentials',
+        ])
+            ->throw();
+        $data = $auth_res->json();
+        $access_token = $data['access_token'];
+
+        $uri = 'https://api.twitch.tv/helix/tags/streams?';
+        foreach ($tags as $tag) {
+            $uri = $uri . 'tag_id=' . $tag . '&';
+        }
+        $uri = substr($uri, 0, -1); // remove extra &
+        $tag_data = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $access_token,
+            'Client-Id' => env('TWITCH_CLIENT_ID'),
+        ])
+            ->get($uri)
+            ->throw();
+        $tag_data = $tag_data->json();
+        $names = [];
+        foreach ($tag_data['data'] as $data) {
+            $names[] = $data['localization_names']['en-us'];
+        }
+        return $names;
+    }
+
+    public function render () {
+        $topStreams = TopStream::all();
+        $userStreams = self::getFollowedStreams();
+
+        // Total number of streams for each game
+        $streamsPerGame = TopStream::selectRaw('count(game_name) as streams, game_name')
+            ->groupBy('game_name')
+            ->pluck('streams', 'game_name');
+
+        // Top games by viewer count for each game
+        $gamesByViewers = TopStream::groupBy('game_name')
+            ->selectRaw('sum(viewers) as viewers, game_name')
+            ->pluck('viewers', 'game_name');
+
+        // Median number of viewers for all streams
+        $medianViews = TopStream::select('viewers')
+            ->pluck('viewers')
+            ->median();
+
+        // List of top 100 streams by viewer count that can be sorted asc & desc
+        $topStreamsAsc = TopStream::orderBy('viewers', 'asc')->get();
+        $topSteamsDesc = TopStream::orderBy('viewers', 'desc')->get();
+
+        // Total number of streams by their start time (rounded to the nearest hour)
+        $streamsPerHr = TopStream::selectRaw('count(*) as streams, date_format(start_time, "%Y-%m-%d %H:00") as time')
+            ->groupByRaw('date_format(start_time, "%Y-%m-%d %H:00")')
+            ->pluck('streams', 'time');
+
+        // Which of the top 1000 streams is the logged in user following?
+        $stream_ids = [];
+        foreach ($userStreams as $user_stream) {
+            $stream_ids[] = $user_stream[0]['id'];
+        }
+        $followedTopStreams = TopStream::whereIn('stream_id', $stream_ids)->get();
+
+        // How many viewers does the lowest viewer count stream that the logged in user is following need to gain in order to make it into the top 100?
+        $needForTop100 = null;
+        if (collect($user_stream)->count() > 0) {
+            $lowest = INF;
+            foreach ($userStreams as $user_stream) {
+                $viewers = $user_stream[0]['viewer_count'];
+                $lowest =  $viewers < $lowest ?  $viewers: $lowest;
+            }
+            $lowest_in_top = TopStream::orderBy('viewers', 'asc')->first()->viewers;
+            $diff = $lowest_in_top - $lowest;
+            $needForTop100 = $diff < 0 ? 0 : $diff;
+        }
+
+        // Which tags are shared between the user followed streams and the top 1000 streams?
+        // Also make sure to translate the tags to their respective name.
+        $user_tags = [];
+        foreach ($userStreams as $user_stream) {
+            $user_tags = array_merge($user_tags, $user_stream[0]['tag_ids'] ?? []);
+        }
+        $user_tags = array_unique($user_tags);
+        $shared_tags = [];
+        foreach ($topStreams as $topStream) {
+            $stream_tags = collect(explode(', ', $topStream->tags));
+            $intersection = $stream_tags->intersect($user_tags);
+            if ($intersection->count()) {
+                $shared_tags = array_merge($shared_tags, $intersection->all());
+            }
+        }
+        $shared_tags = array_unique($shared_tags);
+        $sharedTags = self::getTagNames($shared_tags);
+
+        return Inertia::render('Dashboard', [
+            'userStreams' => $userStreams,
+            'topStreams' => $topStreams,
+            'sharedTags' => $sharedTags,
+            'needForTop100' => $needForTop100,
+            'followedTopStreams' => $followedTopStreams,
+            'streamsPerHr' => $streamsPerHr,
+            'topStreamsAsc' => $topStreamsAsc,
+            'topSteamsDesc' => $topSteamsDesc,
+            'medianViews' => $medianViews,
+            'gamesByViewers' => $gamesByViewers,
+            'streamsPerGame' => $streamsPerGame,
+        ]);
     }
 }
